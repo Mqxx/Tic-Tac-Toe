dbf0de76f4628e5569be3f7928c48816cf4256ae
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-22 15:22:40 +0200

    Update serve_files.ts

--- server/serve_files.ts
+++ server/serve_files.ts
@@ -1,8 +1,26 @@
-export function serverFiles(options: Deno.ServeOptions | Deno.ServeTlsOptions) {
+import { parsePath } from "./routing/parse_path.ts";
+
+export function serverFiles(rootDirectory : string, options: Deno.ServeOptions | Deno.ServeTlsOptions) {
     Deno.serve(
         options,
-        () => {
-            return new Response()
+        async (request) => {
+            const requestURL = new URL(request.url)
+            const defaultPath = parsePath(requestURL.pathname, 'index.html')
+            
+
+            console.log('New request from:', requestURL.pathname, '->', defaultPath);
+            
+
+            try {
+                const file = await Deno.readFile(rootDirectory + defaultPath);
+                return new Response(file);
+            } catch(error) {
+                if (error instanceof Deno.errors.NotFound) {
+                    return new Response('File not found!');
+                } else {
+                    return new Response(error);
+                }
+            }
         }
     );
 }
