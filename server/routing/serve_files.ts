b636e2aa140c81104aaa042e9a0d5fd59355235b
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-22 16:22:17 +0200

    Create serve_files.ts

---
+++ server/routing/serve_files.ts
@@ -0,0 +1,39 @@
+import { contentTypes } from "./content_types.ts";
+import { getContentType } from "./get_content_type.ts";
+import { parsePath } from "./parse_path.ts";
+
+export function serveFiles(
+    directories : {
+        rootDirectory : string,
+        defaultFile : string,
+        notFound : string
+    },
+    options: Deno.ServeOptions | Deno.ServeTlsOptions
+) {
+    Deno.serve(
+        options,
+        async (request) => {
+            const requestURL = new URL(request.url);
+            const defaultPath = parsePath(requestURL.pathname, directories.defaultFile);
+            let contentType = getContentType(defaultPath, '*/*');
+            let file : Uint8Array;
+
+            try {
+                file = await Deno.readFile(directories.rootDirectory + defaultPath);
+            } catch(error) {
+                if (error instanceof Deno.errors.NotFound) {
+                    file = await Deno.readFile(directories.rootDirectory + '/' + directories.notFound);
+                    contentType = contentTypes['html']
+                } else {
+                    file = error
+                    contentType = contentTypes['js']
+                }
+            }
+
+            return new Response(file, {
+                headers: { 'content-type': contentType }
+            });
+        }
+    );
+}
+
