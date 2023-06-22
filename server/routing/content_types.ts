975b0a3ad5a6dc5905843a42204c0b87776be4bf
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-22 16:08:04 +0200

    Create content_types.ts

---
+++ server/routing/content_types.ts
@@ -0,0 +1,8 @@
+export const contentTypes : {[key: string] : string} = {
+    js: 'application/javascript',
+    map: 'application/javascript',
+    json: 'application/json',
+    html: 'text/html',
+    css: 'text/css',
+    less: 'text/css'
+};
