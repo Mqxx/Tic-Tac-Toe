299229db4b15608ba115d0b55d9ac556fd2e67ff
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-22 16:37:20 +0200

    Update get_content_type.ts

--- server/routing/get_content_type.ts
+++ server/routing/get_content_type.ts
@@ -8,7 +8,7 @@ import { contentTypes } from "./content_types.ts";
  */
 export function getContentType(path : string, defaultType : string) : string {
     
-    const fileExtenstion = path.replace(/[^\.]+\.([^\.]+)$/, (_, extension : string) => extension.toLowerCase())
+    const fileExtenstion = path.replace(/[^\.]+\.([^\.]+)$/, (_, extension : string) => extension.toLowerCase());
 
     return contentTypes[fileExtenstion] || defaultType;
 }
