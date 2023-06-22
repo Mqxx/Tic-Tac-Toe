ba646836bfba4bbc9ab9a14f7173c0bba041230b
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-22 15:30:22 +0200

    Update parse_path.ts

--- server/routing/parse_path.ts
+++ server/routing/parse_path.ts
@@ -1,3 +1,15 @@
+/**
+ * This function parses a pathname and appends (if nessesarly) the defaultFile.
+ * ```txt
+ * request/path/name => request/path/name/index.html
+ * request/path/name/ => request/path/name/index.html
+ * request/path/name/file.html => request/path/name/file.html
+ * request/path/style/styles.css => request/path/style/styles.css
+ * ```
+ * @param path The pathname of the path to parse
+ * @param defaultFile The default filename which should be used
+ * @returns The parse pathname
+ */
 export function parsePath(path : string, defaultFile : string) : string {
     return path.replace(/^((?:\/?[^\.\n\/]+)*)\/?$/gm, (_, group1) => `${group1}/${defaultFile}`);
 }
