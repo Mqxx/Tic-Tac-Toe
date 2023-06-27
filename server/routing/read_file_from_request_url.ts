24753c9b00e204b9459903cdaa1a6ee4d84ce7e8
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-27 11:36:20 +0200

    Create read_file_from_request_url.ts

---
+++ server/routing/read_file_from_request_url.ts
@@ -0,0 +1,17 @@
+import { ContentType } from "./content_types.ts";
+import { responseWithContentType } from "./response_with_content_type.ts";
+
+export function readFileFromRequestURL(
+    requestURL : URL,
+    contentType : ContentType
+) : Response {
+    try {
+        return responseWithContentType(requestURL.pathname, contentType);
+    } catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            return responseWithContentType(requestURL.pathname, contentType);
+        } else {
+            return responseWithContentType(requestURL.pathname, contentType);
+        }
+    }
+}
