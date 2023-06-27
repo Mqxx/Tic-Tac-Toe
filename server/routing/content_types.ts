bb57f126edc0d998510a96e4cda3dce9489fadb8
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-27 10:54:43 +0200

    Update content_types.ts

--- server/routing/content_types.ts
+++ server/routing/content_types.ts
@@ -1,8 +1,11 @@
-export const contentTypes : {[key: string] : string} = {
+export const contentTypes = {
     js: 'application/javascript',
     map: 'application/javascript',
     json: 'application/json',
     html: 'text/html',
     css: 'text/css',
     less: 'text/css'
-};
+} as const;
+
+export type ContentType = typeof contentTypes[keyof typeof contentTypes]
+export type ContentKey = keyof typeof contentTypes
