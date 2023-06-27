84fe126b5f4c6f32b3c34edf5041d2b5ae61aa27
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-27 11:34:35 +0200

    Update file_server.ts

--- server/file_server.ts
+++ server/file_server.ts
@@ -1,5 +1,16 @@
+import { getContentType } from "./routing/get_content_type.ts";
+import { readFileFromRequestURL } from "./routing/handle_file_request.ts";
+
 export function fileServer(
-    options: Deno.ServeOptions | Deno.ServeTlsOptions
+    options: Deno.ServeOptions | Deno.ServeTlsOptions,
 ) {
-    
+    Deno.serve(
+        options, 
+        (request) => {
+            return readFileFromRequestURL(new URL(request.url), getContentType(
+                new URL(request.url).pathname,
+                'html'
+            )
+        );
+    });
 }
