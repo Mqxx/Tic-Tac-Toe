d70443ecbfb5efa628ebb9d6f9f890e258c3ebe9
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-27 13:39:22 +0200

    Update response_with_content_type.ts

--- server/routing/response_with_content_type.ts
+++ server/routing/response_with_content_type.ts
@@ -1,6 +1,9 @@
 import { ContentType } from "../type/content_types.ts";
 
-export function responseWithContentType(path : string, contentType : ContentType) {
+export function responseWithContentType(
+    path : string,
+    contentType : ContentType
+) {
     return new Response(
         Deno.readFileSync(path),
         {
