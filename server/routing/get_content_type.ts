157af49d8bd37e2fae54e4adffb834d14b22289b
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-29 14:42:39 +0200

    Update get_content_type.ts

--- server/routing/get_content_type.ts
+++ server/routing/get_content_type.ts
@@ -3,12 +3,12 @@ import { ContentTypeKey, ContentType, contentTypes } from "../type/content_types
 /**
  * This function parses a path and returns the content-type corresponding to the path.
  * @param path The pathname of the path to parse
- * @param defaultType The default content-type which should be used
+ * @param contentType The default content-type which should be used
  * @returns The content-type corresponding to the file
  */
 export function getContentType(
     path : string,
-    defaultType : ContentTypeKey
+    contentType : ContentTypeKey
 ) : ContentType {
     
     const fileExtenstionFromPath = path.replace(
@@ -20,5 +20,5 @@ export function getContentType(
         return contentTypes[fileExtenstionFromPath as ContentTypeKey]
     }
 
-    return contentTypes[defaultType];
+    return contentTypes[contentType];
 }
