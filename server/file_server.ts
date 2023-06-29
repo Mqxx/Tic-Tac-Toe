8bffaed6fec047d14cdbccc360d785b1ce1f40da
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-29 16:07:46 +0200

    Update file_server.ts

--- server/file_server.ts
+++ server/file_server.ts
@@ -13,10 +13,10 @@ export function fileServer(
 ) {
     Deno.serve(
         options, 
-        (request) => {
+        (request) => {            
             const requestURL = new URL(request.url);
             
-            const referer = request.headers.get('referer')
+            const referer = request.headers.get('referer');
 
             let path = ''
 
