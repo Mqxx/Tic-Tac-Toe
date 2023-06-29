3a3ddbac9b7c8d4c924e990272549f452bfc11a2
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-29 14:42:41 +0200

    Update read_file_from_request_url.ts

--- server/routing/read_file_from_request_url.ts
+++ server/routing/read_file_from_request_url.ts
@@ -1,15 +1,16 @@
 /**
- * 
- * @param requestPath The path of the request
+ * This function takes a path and returns the read text file. If the file is not found,
+ * than the fallback path is read. If the fallback path also is not found than an error gets returned.
+ * @param path The path of the request
  * @param fallbackPath The fallback path if the request path is not found
- * @returns 
+ * @returns The read text file.
  */
 export function readFileFromRequestURL(
-    requestPath : string,
+    path : string,
     fallbackPath : string
 ) : Uint8Array {
     try {
-        return Deno.readFileSync(requestPath);
+        return Deno.readFileSync(path);
     } catch (error) {
         if (error instanceof Deno.errors.NotFound) {
             try {
