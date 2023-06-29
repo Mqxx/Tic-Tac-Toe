7b743d9cbb3587a9313cd5fa92147c41717d5c8f
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-29 14:17:55 +0200

    Create path_default_file.ts

---
+++ server/routing/path_default_file.ts
@@ -0,0 +1,23 @@
+/**
+ * This function takes a path and returns the path fith the default file.
+ * If the path is already a path to a file than this path is returned.
+ * ```
+ * pathDefaultFile('path/to/file.txt', 'default_file.txt') // 'path/to/file.txt'
+ * pathDefaultFile('./path/to', 'default_file.txt') // './path/to/default_file.txt'
+ * ```
+ * @param path The path to add the default file to
+ * @param defaultFile The default file
+ * @returns The new path with the default file
+ */
+export function pathDefaultFile(path : string, defaultFile : string) {
+    const regExMatchValidPath = /(.+(?<!\.[^/]+)$)/
+    return path.replace(
+        regExMatchValidPath,
+        (
+            _,
+            path
+        ) => {
+            return path + '/' + defaultFile;
+        }
+    )
+}
