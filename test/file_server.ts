35bfa5fbb02c5bd178df5e75a41c18cb6efb0d90
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-07-07 09:22:48 +0200

    Update file_server.ts

--- test/file_server.ts
+++ test/file_server.ts
@@ -1,20 +1,43 @@
+function getHeader(header : Headers) {
+    const headers : {[key : string] : string} = {};
+    [...header].forEach(([key, value]) => {
+        headers[key] = value
+    });
+
+    return headers;
+}
+
 Deno.serve({
     port: 80
 }, (request) => {
     console.log(request);
-    
-    const headers : {[key : string] : string} = {};
-    [...request.headers].forEach(([key, value]) => {
-        headers[key] = value
-    })
 
     const response = {
+        url: request.url,
+        method: request.method,
         bodyUsed: request.bodyUsed,
-        headers: headers,
-        methode: request.method,
+        cache: request.cache ? request.cache : 'undefinded',
+        credentials: request.credentials ? request.credentials : 'undefined',
+        destination: request.destination ? request.destination : 'undefined',
         redirect: request.redirect ? request.redirect : 'undefined',
-        url: request.url
+        referrer: request.referrer ? request.referrer : 'undefined',
+        referrerPolicy: request.referrerPolicy ? request.referrerPolicy : 'undefined',
+        integrity: request.integrity ? request.integrity : 'undefined',
+        isHistoryNavigation: request.isHistoryNavigation ? request.isHistoryNavigation : 'undefined',
+        isReloadNavigation: request.isReloadNavigation ? request.isReloadNavigation : 'undefined',
+        keepalive: request.keepalive ? request.keepalive : 'undefined',
+        mode: request.mode ? request.mode : 'undefined',
+        signal: {
+            aborted: request.signal.aborted,
+            reason: request.signal.reason ? request.signal.reason : 'undefined'
+        },
+        headers: getHeader(request.headers),
     }
 
-    return new Response(JSON.stringify(response, null, 4));
+    return new Response(JSON.stringify(
+        response,
+        null,
+        4
+    ));
 })
+
